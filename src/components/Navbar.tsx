1000436137157638e792916977b55c1343989c24
SHUSHI <63992333+SHUSHI1137@users.noreply.github.com>
2023-09-29 15:45:36 +0700

    update

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,7 +2,7 @@ import navClass from './Navbar.module.css'
 
 const Navbar = () => {
   return (
-    <nav className={navClass.navstyle}>
+    <nav className={navClass.nav}>
       <p>Navbar</p>
       <button>Login</button>
     </nav>
