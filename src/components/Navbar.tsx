e235554137f450476d025e2d44e8663333c8ff55
SHUSHI <63992333+SHUSHI1137@users.noreply.github.com>
2023-09-29 15:41:47 +0700

    update navbar

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,12 @@
+import navClass from './Navbar.module.css'
+
+const Navbar = () => {
+  return (
+    <nav className={navClass.navstyle}>
+      <p>Navbar</p>
+      <button>Login</button>
+    </nav>
+  )
+}
+
+export default Navbar
