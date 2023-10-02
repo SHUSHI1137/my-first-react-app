c0f15825cb58c38f0fd502283ad6d885ff7e43ae
SHUSHI <63992333+SHUSHI1137@users.noreply.github.com>
2023-10-02 11:26:48 +0700

    update props

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <Navbar />
-      <Greeting />
+      <Greeting name="Steve Roger" greetingMsg="Hello!" isLoggedIn={false} />
     </div>
   )
 }
