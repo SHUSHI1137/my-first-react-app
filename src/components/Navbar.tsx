ac75984a440c8d2accbd876ae5fba0e8fab377b3
SHUSHI <63992333+SHUSHI1137@users.noreply.github.com>
2023-10-02 10:48:59 +0700

    update install tailwind in project

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,10 @@
-import navClass from './Navbar.module.css'
+// import navClass from './Navbar.module.css'
 
 const Navbar = () => {
   return (
-    <nav className={navClass.nav}>
+    <nav className="flex justify-between bg-[#3b82f6] box-border w-full h-auto px-8">
       <p>Navbar</p>
-      <button>Login</button>
+      <button className="rounded-none bg-sky-500/75 h-7 rounded-xl">Login</button>
     </nav>
   )
 }
