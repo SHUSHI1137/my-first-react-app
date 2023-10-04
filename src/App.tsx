38e475cff5eb9286fd6e7af38bb6f894bd3d9db4
SHUSHI <63992333+SHUSHI1137@users.noreply.github.com>
2023-10-04 15:31:49 +0700

    Example useEffect and use axios fetch api

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,38 @@
-import { FormEvent, useState } from 'react'
+import { FormEvent, useEffect, useState } from 'react'
 import './App.css'
 import Greeting from './components/Greeting'
 import Navbar from './components/Navbar'
 import Post from './components/Post'
 import { PostDTO } from './types/dto'
-
-const initialPosts: PostDTO[] = [
-  {
-    id: 1,
-    userId: 1,
-    title: "Let's learn React!",
-    body: 'quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto',
-  },
-  {
-    id: 2,
-    userId: 2,
-    title: 'How to install Node.js',
-    body: 'quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto',
-  },
-  {
-    id: 3,
-    userId: 3,
-    title: 'Basic HTML',
-    body: 'quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto',
-  },
-]
+import axios from 'axios'
 
 function App() {
-  const [posts, setPosts] = useState<PostDTO[]>(initialPosts)
+  const [posts, setPosts] = useState<PostDTO[] | null>(null)
   const [newTitle, setNewTitle] = useState<string>('')
   const [newBody, setNewbody] = useState<string>('')
+  const [isLoading, setIsLoading] = useState<boolean>(false)
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setIsLoading(true)
+      try {
+        const res = await axios.get<PostDTO[]>('https://jsonplaceholder.typicode.com/posts')
+
+        setPosts(res.data)
+      } catch (err) {
+        console.error(err)
+      } finally {
+        setIsLoading(false)
+      }
+    }
+
+    fetchData()
+  }, [])
 
   const handleSubmit = (e: FormEvent) => {
     e.preventDefault()
+
+    if (!posts) return
 
     const currentPosts = [...posts]
 
@@ -49,6 +49,8 @@ function App() {
     setNewTitle('')
     setNewbody('')
   }
+
+  if (isLoading) return <h1>Loading.....</h1>
 
   return (
     <div className="App">
@@ -76,9 +78,10 @@ function App() {
         </button>
       </form>
       <div className="feed-container">
-        {posts.map((post) => {
-          return <Post key={post.id} post={post} />
-        })}
+        {posts &&
+          posts.map((post) => {
+            return <Post key={post.id} post={post} />
+          })}
       </div>
     </div>
   )
